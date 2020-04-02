04e6adb3df7df5bef195e570e7fbfee0368a77e0
Dhaval Dedhia <dhavl32@gmail.com>
2020-04-02 15:10:23 -0400

    Created server

---
+++ services/question.service.server.js
@@ -0,0 +1,7 @@
+
+let questions = require('./questions')
+findQuestionsForQuiz = (quizId) =>
+    questions.filter(question => question.quizId === quizId)
+module.exports = {
+    findQuestionsForQuiz
+}
