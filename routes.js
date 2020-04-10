9372b0900ecaa4a8b2004ef441be449a32e17c4c
Dhaval Dedhia <dhavl32@gmail.com>
2020-04-10 15:27:59 -0400

    Prouction build

--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ exports = module.exports = app => {
   app.get("/courses/:stream", course.getCoursesForStream);
   app.get("/courses", course.getAllCourses);
   app.get("/courses/details/:crn",course.getMoreCourseDetails);
+  
 };
